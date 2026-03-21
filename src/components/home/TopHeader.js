841d825b3c2187121c8fc9034e9bfa5793178122
Med-dev-sl <mohamedsallu24@gmail.com>
2026-03-21 19:22:37 +0000

    Change TopHeader background to deep blue

--- src/components/home/TopHeader.js
+++ src/components/home/TopHeader.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const TopHeader = () => {
   return (
-    <div className="w-full bg-blue-600 text-orange-500 py-4 px-4 md:px-8">
+    <div className="w-full bg-blue-900 text-orange-500 py-4 px-4 md:px-8">
       <div className="max-w-7xl mx-auto">
         {/* Desktop Layout */}
         <div className="hidden md:flex justify-between items-center gap-8">
