1f7a82940ca14505665bfa7c641e43aeca7204be
Med-dev-sl <mohamedsallu24@gmail.com>
2026-03-24 21:57:03 +0000

    added the dark and light mode effect on the screen

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,24 @@
 /** @type {import('tailwindcss').Config} */
 export default {
+  darkMode: 'class',
   content: [
     "./index.html",
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          DEFAULT: '#f97316', // Orange-500
+          light: '#fb923c',
+          dark: '#ea580c',
+        },
+        background: {
+          light: '#ffffff',
+          dark: '#0f172a', // Slate-950
+        }
+      }
+    },
   },
   plugins: [],
 }
